07bf65b94e6d43af26708aa0e47474aec79fa59e
Russ Tyndall <russ@acceleration.net>
2016-12-24 09:53:14 -0500

    Trying to add better meta_data tagging to file saves

--- lib/wraith/helpers/save_metadata.rb
+++ lib/wraith/helpers/save_metadata.rb
@@ -3,13 +3,16 @@ require "wraith"
 class SaveMetadata
   attr_reader :wraith, :history
 
-  def initialize(config, history)
+  def initialize(config, history, file_label)
     @wraith = config
     @history = history
+    @file_label = file_label
   end
 
-  def history_label
-    history ? "_latest" : ""
+  def meta_label
+    return @file_label if @file_label
+    return "_latest" if @history
+    return ""
   end
 
   def file_names(width, label, domain_label)
@@ -18,11 +21,11 @@ class SaveMetadata
   end
 
   def base_label
-    "#{wraith.base_domain_label}#{history_label}"
+    "#{wraith.base_domain_label}#{meta_label}"
   end
 
   def compare_label
-    "#{wraith.comp_domain_label}#{history_label}"
+    "#{wraith.comp_domain_label}#{meta_label}"
   end
 
   def engine
